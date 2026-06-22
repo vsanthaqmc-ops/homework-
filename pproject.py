7b663f9c40adb386121663d8b1a5cd01f9bb0f1c
vsanthaqmc-ops <vsantha.qmc@gmail.com>
2026-06-22 18:55:33 +0530

    Add files via upload

---
+++ pproject.py
@@ -0,0 +1,9 @@
+import math
+
+angle = float(input("Enter angle in degrees: "))
+
+radian = math.radians(angle)
+
+print("Sin =", math.sin(radian))
+print("Cos =", math.cos(radian))
+print("Tan =", math.tan(radian))
